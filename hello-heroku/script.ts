d6f02606d87542cfbae1ab9586de21e52f582fa1
miyi <mylegojesus@gmail.com>
2019-10-30 11:44:42 -0700

    prisma2 dev issue: unable to find database with that name

--- hello-heroku/script.ts
+++ hello-heroku/script.ts
@@ -1,31 +1,26 @@
-import { Photon } from '@generated/photon'
+/**
+  * 
+  * This code was auto-generated based on the introspection result.
+  * Consider it a playground to explore the Photon API.
+  * Feel free to change it as much as you like or delete it altogether.
+  *
+  * The model `User` was randomly selected to demonstrate some API calls.
+  *
+  * Tip: Use the auto-completion of your editor to explore available API operations.
+  * 
+  */
+ 
 
+import { Photon } from '@generated/photon'
 const photon = new Photon()
 
-// A `main` function so that we can use async/await
 async function main() {
-  // Seed the database with users and posts
-  const user1 = await photon.users.create({
-    data: {
-      email: 'alice@prisma.io',
-      name: 'Alice',
-      posts: {
-        create: {
-          title: 'Watch the talks from Prisma Day 2019',
-          content: 'https://www.prisma.io/blog/z11sg6ipb3i1/',
-          published: true,
-        },
-      },
-    },
-    include: {
-      posts: true,
-    },
-  })
+
   const user2 = await photon.users.create({
     data: {
       email: 'bob@prisma.io',
       name: 'Bob',
-      posts: {
+      post: {
         create: [
           {
             title: 'Subscribe to GraphQL Weekly for community news',
@@ -41,52 +36,22 @@ async function main() {
       },
     },
     include: {
-      posts: true,
+      post: true,
     },
   })
-  console.log(`Created users: ${user1.name} (${user1.posts.length} post) and (${user2.posts.length} posts) `)
+  
+  // Tip: Explore some arguments to `findMany` 
+  const allUsers = await photon.users.findMany()
+  console.log(`Retrieved all published users: `, allUsers)
 
-  // Retrieve all published posts
-  const allPosts = await photon.posts.findMany({
-    where: { published: true },
-  })
-  console.log(`Retrieved all published posts: `, allPosts)
-
-  // Create a new post (written by an already existing user with email alice@prisma.io)
-  const newPost = await photon.posts.create({
-    data: {
-      title: 'Join the Prisma Slack community',
-      content: 'http://slack.prisma.io',
-      published: false,
-      author: {
-        connect: {
-          email: 'alice@prisma.io',
-        },
-      },
-    },
-  })
-  console.log(`Created a new post: `, newPost)
-
-  // Publish the new post
-  const updatedPost = await photon.posts.update({
-    where: {
-      id: newPost.id,
-    },
-    data: {
-      published: true,
-    },
-  })
-  console.log(`Published the newly created post: `, updatedPost)
-
-  // Retrieve all posts by user with email alice@prisma.io
-  const postsByUser = await photon.users
-    .findOne({
-      where: {
-        email: 'alice@prisma.io',
-      },
-    })
-    .posts()
-  console.log(`Retrieved all posts from a specific user: `, postsByUser)
+  // Comment out the lines below to create a new User
+  // ATTENTION: This code creates a new record in your database
+  // const newUser = await photon.users.create({
+  //   data: {
+  //     // add some values here
+  //   },
+  // })
+  // console.log(`Created a new User: `, newUser)
 }
 
 main()
